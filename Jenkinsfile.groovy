3f376ab860f8ae878a76a8d067d5ad1e88786a2c
Nelson Abu Samra Rahal Junior <abuzitos@gmail.com>
2019-03-29 18:21:35 -0300

    run stage(Test) with rspec

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,30 +5,40 @@ pipeline {
     stages {
       stage('Initialize')
       {
+        steps {
           sh "echo 'Initialize'"
           sh "docker build ."
+        }
       }
 
       stage('Checkout')
       {
+        steps {
           sh "echo 'Checkout'"
+        }
       }
 
       stage('Build')
       {
+        steps {
           sh "echo 'Build'"
           sh "docker run ruby-app"
+        }
       }
 
       stage('Test')
       {
+        steps {
           sh "echo 'Test'"
           sh "bundle exec rspec spec"
+        }
       }
 
       stage('Deliver')
       {
+        steps {
           sh "echo 'Deliver'"
+        }
       }
     }
 }
