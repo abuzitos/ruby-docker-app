7fa922870e7d2b88b48729240b48cf2a6ac7a945
Nelson Abu Samra Rahal Junior <abuzitos@gmail.com>
2019-03-29 15:45:08 -0300

    +Openshift Conf

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,16 @@
-node(''){
-  stage 'buildInDevelopment'
-  openshiftBuild(namespace: 'development', buildConfig: 'ruby-docker-app', showBuildLogs: 'true')
+node('') {
+  stage('buildInDevelopment') {
+    openshiftBuild(namespace: 'development', buildConfig: 'ruby-docker-app', showBuildLogs: 'true')
+  }
 
-  stage 'deployInDevelopment'
-  openshiftDeploy(namespace: 'development', deploymentConfig: 'ruby-docker-app')
-  openshiftScale(namespace: 'development', deploymentConfig: 'ruby-docker-app',replicaCount: '2')
+  stage('deployInDevelopment') {
+    openshiftDeploy(namespace: 'development', deploymentConfig: 'ruby-docker-app')
+    openshiftScale(namespace: 'development', deploymentConfig: 'ruby-docker-app',replicaCount: '2')
+  }
 
-  stage 'deployInTesting'
-  openshiftTag(namespace: 'development', sourceStream: 'ruby-docker-app',  sourceTag: 'latest', destinationStream: 'ruby-docker-app', destinationTag: 'promoteToQA')
-  openshiftDeploy(namespace: 'testing', deploymentConfig: 'ruby-docker-app', )
-  openshiftScale(namespace: 'testing', deploymentConfig: 'ruby-docker-app',replicaCount: '3')
+  stage('deployInTesting') {
+    openshiftTag(namespace: 'development', sourceStream: 'ruby-docker-app',  sourceTag: 'latest', destinationStream: 'ruby-docker-app', destinationTag: 'promoteToQA')
+    openshiftDeploy(namespace: 'testing', deploymentConfig: 'ruby-docker-app', )
+    openshiftScale(namespace: 'testing', deploymentConfig: 'ruby-docker-app',replicaCount: '3')
+  }
 }
