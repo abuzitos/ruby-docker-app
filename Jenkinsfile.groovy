6dbce30aaa625791addeb02a1df9858783787268
Nelson Abu Samra Rahal Junior <abuzitos@gmail.com>
2019-03-30 10:14:11 -0300

    run stage(Test) with rspec

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,37 @@
-node("docker") {
-    //docker.withRegistry('<<your-docker-registry>>', '<<your-docker-registry-credentials-id>>') {
+node {
+    def app
 
-        git url: "https://github.com/abuzitos/ruby-docker-app.git", credentialsId: 'https://abuzitos:rahal98@gmail.com'
+    stage('Clone repository') {
+        /* Let's make sure we have the repository cloned to our workspace */
 
-        sh "git rev-parse HEAD > .git/commit-id"
-        def commit_id = readFile('.git/commit-id').trim()
-        println commit_id
+        checkout scm
+    }
 
-        stage "build"
-        def app = docker.build "ruby-docker-app"
+    stage('Build image') {
+        /* This builds the actual image; synonymous to
+         * docker build on the command line */
 
-        stage "publish"
-        app.push 'master'
-        app.push "${commit_id}"
+        app = docker.build("abu-projeto/ruby-docker-app")
+    }
+
+    stage('Test image') {
+        /* Ideally, we would run a test framework against our image.
+         * For this example, we're using a Volkswagen-type approach ;-) */
+
+        app.inside {
+            sh 'echo "Tests passed"'
+        }
+    }
+
+    stage('Push image') {
+        /* Finally, we'll push the image with two tags:
+         * First, the incremental build number from Jenkins
+         * Second, the 'latest' tag.
+         * Pushing multiple tags is cheap, as all the layers are reused. */
+        docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
+            app.push("${env.BUILD_NUMBER}")
+            app.push("latest")
+        }
     }
 }
 
